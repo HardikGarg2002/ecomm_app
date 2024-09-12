5c8863a79b039abfc6c3cb484554c4decbd8ef34
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-09-13 01:13:18 +0530

    home page updated and navigation json added

---
+++ lib/categories.ts
@@ -0,0 +1,47 @@
+export const getOccasionsForHomePage = () => {
+  const homepageOccasions = [
+    {
+      _id: "1",
+      title: "Get Well ",
+      url: "/gifts/get-well-soon",
+      img_url:
+        "https://cdn.shopify.com/s/files/1/0741/2259/2535/files/get_well_soon.jpg?v=1718021874",
+    },
+    {
+      _id: "2",
+      title: "New Born",
+      url: "/gifts/new-born",
+      img_url:
+        "https://cdn.shopify.com/s/files/1/0741/2259/2535/files/newborn_acfd1fed-e48d-48f2-870a-57742e81ae4a.jpg?v=1715935166",
+    },
+    {
+      _id: "3",
+      title: "Congrats",
+      url: "/gifts/congratulations",
+      img_url:
+        "https://cdn.shopify.com/s/files/1/0741/2259/2535/files/congratulation_0fec5cfe-616c-4886-980e-01ccb7034427.jpg?v=1715935166",
+    },
+    {
+      _id: "4",
+      title: "Birthday",
+      url: "/gifts/birthday",
+      img_url:
+        "https://cdn.shopify.com/s/files/1/0741/2259/2535/files/birthday_ed4662b1-dbe8-48fd-bbb3-726418b6528a.jpg?v=1715935166",
+    },
+    {
+      _id: "5",
+      title: "Flowers",
+      url: "/flowers",
+      img_url:
+        "https://cdn.shopify.com/s/files/1/0741/2259/2535/files/flowers_0821394c-50ae-4c10-aeb6-4e6a4ae8a5f8.jpg?v=1715934576",
+    },
+    {
+      _id: "6",
+      title: "Anniversary",
+      url: "/gifts/anniversary",
+      img_url:
+        "https://cdn.shopify.com/s/files/1/0741/2259/2535/files/anniversary_6b83b638-3908-46cb-b59a-b513d9201f6b.jpg?v=1715935166",
+    },
+  ];
+  return homepageOccasions;
+};
