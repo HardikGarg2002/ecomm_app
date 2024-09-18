28b33f2295b5af4c3cdfd1b6ec5039073e868ee8
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-09-18 23:22:39 +0530

    added routing for product detail and listing page

--- lib/type/navigation.ts
+++ lib/type/navigation.ts
@@ -1,3 +1,5 @@
+import { IProduct } from "./product";
+
 export interface INavigation {
   _id?: string;
   label: string;
@@ -17,3 +19,11 @@ export interface INavigationItem {
   label: string;
   url: string;
 }
+
+// src/types/navigation.ts (or wherever you prefer to place it)
+export type RootStackParamList = {
+  ProductDetail: { product: IProduct };
+  MainTabs: any;
+  ProductListing: { categorySlug: any };
+  // Other routes can be added here
+};
